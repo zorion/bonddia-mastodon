4f2bdfc67531a7e7d6b70b0f2b86abfac323b79c
ZK Arrizabalaga <zarrizabalaga@g-n.com>
2025-01-22 12:46:21 +0100

    Fes servir idiomes.

--- src/bonddia.py
+++ src/bonddia.py
@@ -1,13 +1,15 @@
+from __future__ import annotations
 import os
+import random
+
 import requests
-import datetime
 
 # Variables d'entorn
 API_BASE_URL = os.getenv("MASTODON_API_BASE_URL")
 ACCESS_TOKEN = os.getenv("MASTODON_ACCESS_TOKEN")
 
 
-def envia(missatge):
+def envia(missatge: str):
     url = f"{API_BASE_URL}/api/v1/statuses"
 
     # Headers per l'autenticació
@@ -25,6 +27,36 @@ def envia(missatge):
         print(response.json())
 
 
+def obtenir_missatge(text: str, idioma: str) -> str:
+    missatge = f"""{text}
+
+#BonDia
+Idioma: #{idioma}
+#BondDia
+    """
+    print(f"Missatge: ----\n{missatge}\n-------------------")
+    return missatge
+
+
+def selecciona_idioma(traduccions: dict[str]) -> tuple[str, str]:
+    idiomes = list(traduccions.keys())
+    idioma = random.choice(idiomes)
+    return idioma, traduccions[idioma]
+
+
+def obtenir_traduccions() -> str:
+    try:
+        from .lang import get_text, parse_lang
+
+        html_data = get_text()
+        return parse_lang(html_data)
+    except Exception as err:
+        print(f"Error aconseguint les traduccions: {err}")
+        return {"catalan": "Bon dia"}
+
+
 if __name__ == "__main__":
-    missatge = f"BONDDIA {datetime.date.today().isoformat()}"
+    traduccions = obtenir_traduccions()
+    idioma, bondia = selecciona_idioma(traduccions)
+    missatge = obtenir_missatge(text=bondia, idioma=idioma)
     envia(missatge)
--- src/lang.py
+++ src/lang.py
@@ -1,7 +1,71 @@
+from html.parser import HTMLParser
 import requests
-URL_SOURCE = 'http://www.rogerdarlington.me.uk/Goodmorning.html'
+
+URL_SOURCE = "http://www.rogerdarlington.me.uk/Goodmorning.html"
+CACHE_FILE = "bondia.html"
+
 
 def get_text():
-    response = requests.get(URL_SOURCE)
-    response.raise_for_status()
-    return response.text
+    try:
+        response = requests.get(URL_SOURCE)
+        response.raise_for_status()
+        text = response.text
+        with open(CACHE_FILE, "w") as f:
+            f.write(text)
+    except Exception as err:
+        print(f"Error baixant el document: {err}, fent servir cache.")
+        with open(CACHE_FILE, "r") as f:
+            text = f.read()
+    return text
+
+
+def parse_lang(text):
+    lp = LangParser()
+    lp.feed(text)
+    result = lp.get_result()
+
+    return result
+
+
+STATUS_INITIAL = "Abans de la taula"
+STATUS_READING = "Llegint"
+STATUS_GETTING_LANG = "Llegint nou idioma"
+STATUS_LANGUAGE_GOT = "Llegint per al idioma"
+STATUS_FINISHED = "Acabada la taula"
+
+
+class LangParser(HTMLParser):
+    def __init__(self):
+        self.__status = STATUS_INITIAL
+        self.__lang = None
+        self.__result = {}
+        super().__init__()
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "table" and self.__status == STATUS_INITIAL:
+            self.__status = STATUS_READING
+        if tag == "td":
+            if self.__status == STATUS_READING:
+                self.__status = STATUS_GETTING_LANG
+            elif self.__status == STATUS_GETTING_LANG:
+                self.__status = STATUS_LANGUAGE_GOT
+                assert self.__lang is not None
+
+    def handle_endtag(self, tag):
+        if tag == "table":
+            self.__status = STATUS_FINISHED
+
+    def handle_data(self, data):
+        if self.__status == STATUS_LANGUAGE_GOT:
+            lang = self.__lang.strip()
+            bondia = data.strip()
+            self.__result[lang] = bondia
+            self.__lang = None
+            self.__status = STATUS_READING
+
+        if self.__status == STATUS_GETTING_LANG:
+            assert self.__lang is None
+            self.__lang = data
+
+    def get_result(self):
+        return self.__result
